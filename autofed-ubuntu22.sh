5291b9bf8373d0a9815fe284f034bef5ff183fbf
Shlee <github@shl.ee>
2022-06-28 00:24:04 +0930

    Update autofed-ubuntu22.sh

--- autofed-ubuntu22.sh
+++ autofed-ubuntu22.sh
@@ -4,7 +4,39 @@ RootPass=""
 PixelfedDBpass=""
 
 main() {
-  echo "demo"
+
+    if [ ! -t 1 ]; then
+        err "Autofed has to run interactively"
+    fi
+
+    echo "Running Autofed for Ubuntu 22.04"
+    install_redis || return 1
+    install_mariadb || return 1
+    #mysql_secure_installation || return 1
+}
+
+err() {
+    >&2 echo "$1"
+    exit 1
 }
 
 main "$@" || exit 1
+
+
+install_redis() {
+    apt -y install redis-server
+    systemctl enable --now redis-server
+}
+
+install_mariadb() {
+    apt -y install mariadb-server
+    systemctl enable --now mariadb
+}
+
+mysql_secure_installation() {
+    ## mysql_secure_installation
+}
+
+
+
+
