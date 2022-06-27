0e29faa517393f3d1f1175d930e1e147cd78730b
Shlee <github@shl.ee>
2022-06-28 00:37:13 +0930

    Update autofed-ubuntu22.sh

--- autofed-ubuntu22.sh
+++ autofed-ubuntu22.sh
@@ -29,41 +29,49 @@ err() {
 }
 
 apt_update() {
+    echo "apt_update"
     apt update
 }
 
 install_redis() {
+    echo "install_redis"
     apt -y install redis-server
     systemctl enable --now redis-server
 }
 
 install_mariadb() {
+    echo "install_mariadb"
     apt -y install mariadb-server
     systemctl enable --now mariadb
 }
 
 mysql_secure_installation() {
+    echo "mysql_secure_installation"
     echo "TODO"
 }
 
 prepare_db() {
+    echo "prepare_db"
     mysql -u root <<EOS
     create database pixelfed;
     grant all privileges on pixelfed.* to 'pixelfed'@'localhost' identified by "${PixelfedDBpass}";
     flush privileges;
-    EOS
+EOS
 }
 
 install_packages() {
+    echo "install_packages"
     apt -y install ffmpeg unzip zip jpegoptim optipng pngquant gifsicle
 }
 
 install_PHP_packages() {
+    echo "install_PHP_packages"
     apt -y install php8.1-fpm php8.1-cli
     apt -y install php8.1-bcmath php8.1-curl php8.1-gd php8.1-intl php8.1-mbstring php8.1-xml php8.1-zip php8.1-mysql php-redis
 }
 
 configure_PHP_inis() {
+    echo "configure_PHP_inis"
     sed -i "s/post_max_size = .*/post_max_size = 300M/g" /etc/php/8.1/cli/php.ini
     sed -i "s/file_uploads = .*/file_uploads = On/g" /etc/php/8.1/cli/php.ini
     sed -i "s/upload_max_filesize = .*/upload_max_filesize = 300M/g" /etc/php/8.1/cli/php.ini
@@ -78,7 +86,7 @@ configure_PHP_inis() {
 }
 
 configure_FPM_inis() {
-    echo "TODO"
+    echo "configure_FPM_inis"
     cp /etc/php/8.1/fpm/pool.d/www.conf /etc/php/8.1/fpm/pool.d/pixelfed.conf
 #     sed -i "s/"\[www\]"/"\[pixelfed\]"/g" /etc/php/8.1/fpm/pool.d/pixelfed.conf
 #     sed -i "s/user = .*/user = pixelfed/g" /etc/php/8.1/fpm/pool.d/pixelfed.conf
@@ -88,6 +96,7 @@ configure_FPM_inis() {
 }
 
 install_composer() {
+    echo "install_composer"
     curl -sS https://getcomposer.org/installer -o /tmp/composer-setup.php
     php /tmp/composer-setup.php --install-dir=/usr/local/bin --filename=composer
 }
