d864cfa8576b0622ddb1de3d26b52322ba8f4bcb
Shlee <github@shl.ee>
2022-06-28 00:55:11 +0930

    Update autofed-ubuntu22.sh

--- autofed-ubuntu22.sh
+++ autofed-ubuntu22.sh
@@ -108,7 +108,7 @@ configure_FPM_inis() {
     fancyecho "-----------------------------------------"
     fancyecho "configure_FPM_inis"
     cp /etc/php/8.1/fpm/pool.d/www.conf /etc/php/8.1/fpm/pool.d/pixelfed.conf
-#     sed -i "s/"\[www\]"/"\[pixelfed\]"/g" /etc/php/8.1/fpm/pool.d/pixelfed.conf
+#     sed -i "s/"[www]"/"[pixelfed]"/g" /etc/php/8.1/fpm/pool.d/pixelfed.conf
 #     sed -i "s/user = .*/user = pixelfed/g" /etc/php/8.1/fpm/pool.d/pixelfed.conf
 #     sed -i "s/group = .*/group = pixelfed/g" /etc/php/8.1/fpm/pool.d/pixelfed.conf
 #     sed -i "s/listen = .*/listen = \/run\/php\/php8.1-fpm-pixelfed.sock/g" /etc/php/8.1/fpm/pool.d/pixelfed.conf
