8d1a229e4a2126edfec640f3df403cd0fd6056bb
Shlee <github@shl.ee>
2022-07-05 21:36:28 +0930

    Update autofed-ubuntu22.sh

--- autofed-ubuntu22.sh
+++ autofed-ubuntu22.sh
@@ -48,8 +48,8 @@ fancyecho() {
 ## steps by Shlee
 autofed_variables() {
     fancyecho "-----------------------------------------"
-    fancyecho "apt_update"
-    fancyecho "--"
+    fancyecho "autofed_variables"
+    fancyecho "-----------------------------------------"
     fancyecho "Press enter to generate random passwords"
     read -r -p 'PFDomain (pixelfed.au): ' PFDomain
     read -r -p 'PFDomainEmail (pixelfed@pixelfed.au): ' PFDomainEmail
@@ -73,18 +73,21 @@ autofed_variables() {
 apt_update() {
     fancyecho "-----------------------------------------"
     fancyecho "apt_update"
+    fancyecho "-----------------------------------------"
     apt update
 }
 
 adduser_pixelfed() {
     fancyecho "-----------------------------------------"
     fancyecho "adduser_pixelfed"
+    fancyecho "-----------------------------------------"
     adduser --disabled-password --gecos "" pixelfed
 }
 
 install_redis() {
     fancyecho "-----------------------------------------"
     fancyecho "install_redis"
+    fancyecho "-----------------------------------------"
     apt -y install redis-server
     systemctl enable --now redis-server
 }
@@ -92,6 +95,7 @@ install_redis() {
 install_mariadb() {
     fancyecho "-----------------------------------------"
     fancyecho "install_mariadb"
+    fancyecho "-----------------------------------------"
     apt -y install mariadb-server
     systemctl enable --now mariadb
 }
@@ -100,12 +104,14 @@ install_mariadb() {
 mysql_secure() {
     fancyecho "-----------------------------------------"
     fancyecho "mysql_secure_installation"
+    fancyecho "-----------------------------------------"
     /usr/bin/mysql_secure_installation
 }
 
 prepare_db() {
     fancyecho "-----------------------------------------"
     fancyecho "prepare_db"
+    fancyecho "-----------------------------------------"
     mysql -u root <<EOS
     create database pixelfed;
     grant all privileges on pixelfed.* to 'pixelfed'@'localhost' identified by "${DBPixelfedPass}";
@@ -116,12 +122,14 @@ EOS
 install_packages() {
     fancyecho "-----------------------------------------"
     fancyecho "install_packages"
+    fancyecho "-----------------------------------------"
     apt -y install ffmpeg unzip zip jpegoptim optipng pngquant gifsicle
 }
 
 install_PHP_packages() {
     fancyecho "-----------------------------------------"
     fancyecho "install_PHP_packages"
+    fancyecho "-----------------------------------------"
     apt  -y install php8.1-fpm php8.1-cli
     systemctl enable --now php8.1-fpm
     apt  -y install php8.1-bcmath php8.1-curl php8.1-gd php8.1-intl php8.1-mbstring php8.1-xml php8.1-zip php8.1-mysql php-redis
@@ -130,6 +138,7 @@ install_PHP_packages() {
 configure_PHP_inis() {
     fancyecho "-----------------------------------------"
     fancyecho "configure_PHP_inis"
+    fancyecho "-----------------------------------------"
     sed -i 's/^post_max_size = .*/post_max_size = 300M/' /etc/php/8.1/cli/php.ini
     sed -i 's/^file_uploads = .*/file_uploads = On/' /etc/php/8.1/cli/php.ini
     sed -i 's/^upload_max_filesize = .*/upload_max_filesize = 300M/' /etc/php/8.1/cli/php.ini
@@ -146,6 +155,7 @@ configure_PHP_inis() {
 configure_FPM_inis() {
     fancyecho "-----------------------------------------"
     fancyecho "configure_FPM_inis"
+    fancyecho "-----------------------------------------"
     cp /etc/php/8.1/fpm/pool.d/www.conf /etc/php/8.1/fpm/pool.d/pixelfed.conf
     sed -i 's/\[www\]/[pixelfed]/' /etc/php/8.1/fpm/pool.d/pixelfed.conf
     sed -i 's/^user = .*/user = pixelfed/' /etc/php/8.1/fpm/pool.d/pixelfed.conf
@@ -157,6 +167,7 @@ configure_FPM_inis() {
 install_composer() {
     fancyecho "-----------------------------------------"
     fancyecho "install_composer"
+    fancyecho "-----------------------------------------"
     curl -sS https://getcomposer.org/installer -o /tmp/composer-setup.php
     php /tmp/composer-setup.php --install-dir=/usr/local/bin --filename=composer
 }
@@ -164,6 +175,7 @@ install_composer() {
 git_clone() {
     fancyecho "-----------------------------------------"
     fancyecho "git_clone"
+    fancyecho "-----------------------------------------"
     runuser - pixelfed -c "git clone -b dev https://github.com/pixelfed/pixelfed.git pixelfed"
     runuser - pixelfed -c "cd pixelfed && composer install --no-ansi --no-interaction --optimize-autoloader"
 }
@@ -171,12 +183,14 @@ git_clone() {
 artisan_install() {
     fancyecho "-----------------------------------------"
     fancyecho "artisan_install"
+    fancyecho "-----------------------------------------"
     runuser - pixelfed -c "cd pixelfed && php artisan install"
 }
 
 artisan_horizon() {
     fancyecho "-----------------------------------------"
     fancyecho "artisan_horizon"
+    fancyecho "-----------------------------------------"
     runuser - pixelfed -c "cd pixelfed && php artisan horizon:install"
     runuser - pixelfed -c "cd pixelfed && php artisan horizon:publish"
 }
@@ -190,6 +204,7 @@ set_pathpermissions() {
 install_nginx() {
     fancyecho "-----------------------------------------"
     fancyecho "install_nginx"
+    fancyecho "-----------------------------------------"
     apt -y install nginx certbot python3-certbot-nginx
     systemctl enable --now nginx
 }
@@ -198,6 +213,7 @@ install_nginx() {
 nginx_certbot() {
     fancyecho "-----------------------------------------"
     fancyecho "nginx_certbot"
+    fancyecho "-----------------------------------------"
     rm /etc/nginx/sites-enabled/default
     certbot certonly --standalone --noninteractive --agree-tos -m ${PFDomainEmail} -d ${PFDomain}
     cp /home/pixelfed/pixelfed/contrib/nginx.conf /etc/nginx/sites-available/pixelfed.conf
@@ -215,6 +231,7 @@ nginx_certbot() {
 systemd_pixelfedhorizon() {
     fancyecho "-----------------------------------------"
     fancyecho "systemd_pixelfedhorizon"
+    fancyecho "-----------------------------------------"
     
 tee /etc/systemd/system/pixelfedhorizon.service <<EOF
 [Unit]
@@ -248,11 +265,11 @@ EOF
 cron_artisan_schedule() {
     fancyecho "-----------------------------------------"
     fancyecho "cron_artisan_schedule"
+    fancyecho "-----------------------------------------"
     croncmd="/usr/bin/php /home/pixelfed/pixelfed/artisan schedule:run >> /dev/null 2>&1"
     cronjob="* * * * * $croncmd"
     ( crontab -l | grep -v -F "$croncmd" ; echo "$cronjob" ) | crontab -
 }
-
 
 ## main always at the bottom
 main "$@" || exit 1
